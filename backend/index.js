e46583244d2cc2de6db6b4dee35a0bc3396d7bac
Tanya Katiyar <tanya100katiyar@gmail.com>
2024-09-26 16:32:32 +0530

    updating index.js

--- backend/index.js
+++ backend/index.js
@@ -240,7 +240,7 @@ app.listen(PORT, () => {
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+  res.header("Access-Control-Allow-Origin", "https://zerodhan.onrender.com"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
