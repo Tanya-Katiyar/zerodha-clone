e745d27ae9e336f7e2ba0a09a15a91ebdddf0e95
Tanya Katiyar <tanya100katiyar@gmail.com>
2024-09-30 14:55:42 +0530

    removing frintend link

--- backend/index.js
+++ backend/index.js
@@ -241,11 +241,7 @@ app.listen(PORT, () => {
 
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://zerodhan.onrender.com"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(express.json());
 
